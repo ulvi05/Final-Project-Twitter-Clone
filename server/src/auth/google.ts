419d80d9317539ab48f09c3506b388787b58737c
ulvi05 <aghazadeulvi@gmail.com>
2025-01-27 17:01:09 +0400

    feat(server): google auth some features added

--- server/src/auth/google.ts
+++ server/src/auth/google.ts
@@ -1,20 +1,53 @@
-// import passport from "passport";
-// import dotenv from "dotenv";
-// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import passport from "passport";
+import dotenv from "dotenv";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../mongoose/schema/user";
+import { IUser } from "../types/user";
 
-// dotenv.config();
+dotenv.config();
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: GOOGLE_CLIENT_ID,
-//       clientSecret: GOOGLE_CLIENT_SECRET,
-//       callbackURL: "http://www.example.com/auth/google/callback",
-//     },
-//     function (accessToken, refreshToken, profile, cb) {
-//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//         return cb(err, user);
-//       });
-//     }
-//   )
-// );
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        const newUser = new User({
+          googleId: profile.id,
+          email: profile.emails?.[0]?.value,
+          name: profile.displayName,
+          avatar: profile.photos?.[0]?.value,
+        });
+
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user: any, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      throw new Error("User not found");
+    }
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
